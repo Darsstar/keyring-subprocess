054fb513ed7e727ce0f920d73d33535673ef142b
Dos Moonen <darsstar@gmail.com>
2022-07-20 17:26:44 +0200

    Handle virtualenv being vendored

--- src/keyring_subprocess/_internal/__init__.py
+++ src/keyring_subprocess/_internal/__init__.py
@@ -6,8 +6,15 @@ except ImportError:
 
     sys.meta_path.append(KeyringSubprocessFinder())
 
+
 try:
-    import virtualenv
+    from importlib import import_module
+
+    # if keyring-subprocess is vendored try to import vendored virtualenv
+    vendor_prefix_parts = list(__name__.split(".")[:-2])
+    vendored_virtualenv = vendor_prefix_parts + ["virtualenv"]
+    vendored_virtualenv = ".".join(vendored_virtualenv)
+    import_module(vendored_virtualenv)
     from ._seeder import KeyringSubprocessFromAppData
 except ImportError:
     pass
